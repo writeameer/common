dbec76eee4c6a2fc5b546d615489402bcfafb78a
Ameer Deen <ameer.deen@nab.com.au>
2017-11-27 13:52:27 +1100

    Adding Splunk Logging Lib

---
+++ splunk.go
@@ -0,0 +1,55 @@
+package common
+
+import (
+	"fmt"
+	"net/http"
+	"crypto/tls"
+	"io/ioutil"
+	"bytes"
+	"encoding/json"	
+)
+
+type SplunkResponse struct {
+	Text       string `json:"text"`      
+	Code    int `json:"code"`   
+}
+
+func PostDataToSplunk(jsonData string, splunkServer string, splunkToken string) {
+
+	// Define http request object
+	postData := []byte(`{"event":`+ jsonData + `}`)
+	postUri := "/services/collector"
+	url := splunkServer + postUri
+	req, err := http.NewRequest("POST",url,bytes.NewBuffer(postData))
+
+	// Set Auth Header for requesy
+	authHeader := "Splunk " + splunkToken
+	fmt.Println(authHeader)
+	req.Header.Set("Authorization", authHeader)
+	req.Header.Set("Content-Type", "application/json")
+
+	// Create HTTP client with no SSL check (self signed certs)
+	transport := &http.Transport{
+        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: transport}
+
+	// Post request
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+	}
+	
+    defer resp.Body.Close()
+
+	// Output response
+	data , _ := ioutil.ReadAll(resp.Body)
+
+	// Convert json response to struct
+	var response SplunkResponse
+	err = json.Unmarshal(data, &response)
+
+	// Output response
+	fmt.Println("Response Code:", response.Code)
+	fmt.Println("Response Text:", response.Text)
+}
